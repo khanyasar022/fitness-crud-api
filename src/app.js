ccff844fd0b1af8c1f9a53900c6ad008a5ee04a3
khanyasar022 <38328077+khanyasar022@users.noreply.github.com>
2022-02-27 14:10:43 +0530

    removed body-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const cookieParser = require("cookie-parser")
 
@@ -11,8 +10,8 @@ const userRoutes = require('./api/users/routes')
 //middlewares
 app.use(cookieParser())
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //routes
 app.use('/users', userRoutes)
