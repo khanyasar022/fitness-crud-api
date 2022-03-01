e616a7647bd8716784f3c67200a2813b2ef8359b
khanyasar022 <38328077+khanyasar022@users.noreply.github.com>
2022-03-01 16:41:47 +0530

    removed try catch blocks from service functions

--- src/services/rsvps.js
+++ src/services/rsvps.js
@@ -1,21 +1,13 @@
 const Rsvp = require('../models/rsvpSchema')
 
 const createRsvp = async (data) => {
-    try {
         const createdRsvp = await Rsvp.create(data)
-        return createRsvp
-    } catch(error) {
-        console.log(`create rsvp error ${error}`)
-    }
+        return createdRsvp
 }
 
 const getRsvp = async (userId, eventId) => {
-    try {
         const allRsvp = await Rsvp.findOne({ userId: userId, eventId: eventId}).populate('eventId').populate('userId')
         return allRsvp
-    } catch(error) {
-        console.log(`get rsvp error ${error}`)
-    }
 }
 
 const RsvpServices = {
